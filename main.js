1b2aec9ab560d9479c8af632cde7239d5b8f7c74
Bergson Mulato <bergsoncosta@gmail.com>
2023-01-18 22:22:47 -0300

    toca som pom e clap com onclick e retirada do alerta com js inline

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom
+
+function tocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+
 //alert('Olá mundo');
 document.querySelector('.tecla_pom');
 
