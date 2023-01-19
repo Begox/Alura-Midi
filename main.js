cc10eaaa6706cea32be81e179a22cf8ccb2fa22d
Bergson Mulato <bergsoncosta@gmail.com>
2023-01-18 22:48:59 -0300

    lista das teclas

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelectorAll(‘.tecla’).play();
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
 /*
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
